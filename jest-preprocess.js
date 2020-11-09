52c174427a148058259fe86a706baca4623bba1b
George Richards <georgejjrichards@gmail.com>
2020-11-09 16:36:08 +0000

    changed name of package and removed other unused stuff

--- jest-preprocess.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const babelOptions = {
-  presets: ['babel-preset-gatsby'],
-};
-
-module.exports = require('babel-jest').createTransformer(babelOptions);
